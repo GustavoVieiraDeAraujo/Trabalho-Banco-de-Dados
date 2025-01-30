9aeab3d2835798997e9caddf0da2940bafeebf4e
Felipe Costa de Sousa <felipecostadesousa.sigma@gmail.com>
2025-01-30 18:40:59 -0300

    tabela jogador

--- sql/tabela_jogador.sql
+++ sql/tabela_jogador.sql
@@ -4,18 +4,18 @@ CREATE TABLE Jogador (
   apelido VARCHAR(255),
   data_nascimento DATE,
   nacionalidade VARCHAR(255),
-  imagemURL VARCHAR(255)
+  imagemURL VARCHAR(255),
   posicao VARCHAR(255),
   altura DOUBLE PRECISION,
-  pe_dominante VARCHAR(10) CHECK (pe_dominante IN ('Esquerda', 'Direita', 'Ambos')),
+  pe_dominante VARCHAR(10) CHECK (pe_dominante IN ('Esquerdo', 'Direito', 'Ambos')),
   valor_mercado MONEY,
-  cidade_nascimento VARCHAR(255)
+  cidade_nascimento VARCHAR(255),
   numero_camisa INTEGER,
-  agente VARCHAR(255)
+  agente VARCHAR(255),
   patrocinador VARCHAR(255),
   redes_sociais VARCHAR(255),
   contrato_incio DATE,
   contrato_fim DATE,
   id_time INT,
-  CONSTRAINT fk_time FOREIGN KEY (id_time) REFERENCES Time(id),
+  CONSTRAINT fk_time FOREIGN KEY (id_time) REFERENCES Time(id)
 );
